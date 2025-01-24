eb471df418080ab6846524ac69ffaf660c16946c
Jerry Jacob <jacobj2938@gmail.com>
2025-01-23 19:19:43 -0500

    Create Database class

---
+++ Database.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Database {
+    private List<Movie> movies;
+    Database() {
+        movies = new ArrayList<>();
+        movies.add(new Movie("The Godfather", 1972));
+        movies.add(new Movie("Pulp Fiction", 1994));
+        movies.add(new Movie("Inception", 2010));
+        movies.add(new Movie("The Matrix", 1999));
+        movies.add(new Movie("Psycho", 1960));
+    }
+}
